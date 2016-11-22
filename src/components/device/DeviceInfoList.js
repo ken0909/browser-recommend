b7c22842af9297c3e75ae9e96850784d01639e72
ken0909 <shiva0909ken@gmail.com>
2016-11-22 22:54:40 +0900

    fix DeviceInfoList component

--- src/components/device/DeviceInfoList.js
+++ src/components/device/DeviceInfoList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Card } from 'material-ui/Card';
 import { List, ListItem } from 'material-ui/List';
@@ -6,57 +6,70 @@ import Divider from 'material-ui/Divider';
 import Header from '../header/Header';
 import { firebaseDbRef } from '../../util/FirebaseUtil';
 
-const deviceList = [];
-firebaseDbRef('device').on('value', (snapshot) => {
-    snapshot.forEach((data) => {
-        deviceList.push({
-            browser: data.val().browser,
-            platform: data.val().platform,
-            userAgent: data.val().userAgent,
-        });
-    });
-});
-
-const style = {
-    card: {
-        margin: '10px 0px',
+export default class DevieInfoList extends Component {
+    constructor() {
+        super();
+        this.state = { deviceInfoList: [] };
+        this.style = {
+            card: {
+                margin: '10px 0px',
+            }
+        };
+        if (window.innerWidth >= 1140) {
+            this.style.root = {
+                margin: '0 15%',
+            };
+        }
     }
-};
-if (window.innerWidth >= 1140) {
-    style.root = {
-        margin: '0 15%',
-    };
+
+    componentDidMount() {
+        this.listenForItem(firebaseDbRef('device'));
+    }
+
+    listenForItem(dbRef) {
+        dbRef.on('value', (snapshot) => {
+            const deviceInfoList = [];
+            snapshot.forEach((data) => {
+                deviceInfoList.push({
+                    browser: data.val().browser,
+                    platform: data.val().platform,
+                    userAgent: data.val().userAgent,
+                });
+            });
+            this.setState({ deviceInfoList });
+        });
+    }
+
+    render() {
+        return (
+            <MuiThemeProvider>
+                <div className="DeviceInfoList" style={this.style.root}>
+                    <Header />
+                    {this.state.deviceInfoList.map((data, index) => (
+                        <Card style={this.style.card} key={index}>
+                            <List>
+                                <ListItem
+                                    primaryText={data.browser}
+                                    disableKeyboardFocus={true}
+                                    disabled={true}
+                                />
+                                <Divider />
+                                <ListItem
+                                    primaryText={data.platform}
+                                    disableKeyboardFocus={true}
+                                    disabled={true}
+                                />
+                                <Divider />
+                                <ListItem
+                                    primaryText={data.userAgent}
+                                    disableKeyboardFocus={true}
+                                    disabled={true}
+                                />
+                            </List>
+                        </Card>
+                    ))}
+                </div>
+            </MuiThemeProvider>
+        );
+    }
 }
-
-const DeviceInfoList = () => (
-    <MuiThemeProvider>
-        <div className="DeviceInfoList" style={style.root}>
-            <Header />
-            {deviceList.map((data, index) => (
-                <Card style={style.card} key={index}>
-                    <List>
-                        <ListItem
-                            primaryText={data.browser}
-                            disableKeyboardFocus={true}
-                            disabled={true}
-                        />
-                        <Divider />
-                        <ListItem
-                            primaryText={data.platform}
-                            disableKeyboardFocus={true}
-                            disabled={true}
-                        />
-                        <Divider />
-                        <ListItem
-                            primaryText={data.userAgent}
-                            disableKeyboardFocus={true}
-                            disabled={true}
-                        />
-                    </List>
-                </Card>
-            ))}
-        </div>
-    </MuiThemeProvider>
-);
-
-export default DeviceInfoList;
